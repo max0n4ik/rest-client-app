b3b5f78ce1ca942620bae387a2cdf23630bd749c
Asya Danilova <asya_danilova@epam.com>
2025-09-12 11:39:07 +0300

    feat: incognito-home-page and authorized-home-page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Footer from '@/shared/Footer/Footer';
 import Header from '@/shared/Header/Header';
 import IncognitoHome from './IncognitoHome';
+import AuthorizedHome from './AuthorizedHome';
 
 export default function Home() {
+  const randomNumber = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
   return (
     <>
       <Header />
-      <IncognitoHome />
+      {randomNumber % 2 === 0 ? <IncognitoHome /> : <AuthorizedHome />}
       <Footer />
     </>
   );
---
+++ src/shared/UserCard/UserCard.tsx
@@ -0,0 +1,23 @@
+import type { JSX } from 'react';
+
+type UserCardProps = {
+  photo: string;
+  name: string;
+  description: string;
+};
+
+const UserCard = ({ photo, name, description }: UserCardProps): JSX.Element => {
+  return (
+    <li className="flex flex-col items-center text-center">
+      <img
+        src={photo}
+        alt={name}
+        className="mb-4 h-[197px] w-[197px] rounded-full object-cover"
+        style={{ boxShadow: '0 5px 10px rgba(0,0,0,0.10)' }}
+      />
+      <h2 className="mb-2 text-2xl font-semibold">{name}</h2>
+      <p className="text-base">{description}</p>
+    </li>
+  );
+};
+export default UserCard;
