088acdc3697e4df3c3b03c111c0974bdbe2f95e0
Asya Danilova <asya_danilova@epam.com>
2025-09-12 10:49:50 +0300

    feat: incognito-home-page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import Footer from '@/shared/Footer/Footer';
 import Header from '@/shared/Header/Header';
+import IncognitoHome from './IncognitoHome';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <IncognitoHome />
+      <Footer />
     </>
   );
 }
