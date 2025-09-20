09095a2a6394534eed3c90b7738fb1754350368c
max0n4ik <max2004.gorohov@yandex.ru>
2025-09-20 19:40:41 +0300

    feat: add history page

--- src/hooks/useRestClient.ts
+++ src/hooks/useRestClient.ts
@@ -1,3 +1,4 @@
+import { createClient } from '@/api/client';
 import { useState } from 'react';
 
 export interface RestResponse<T = unknown> {
@@ -10,7 +11,7 @@ export function useRestClient<T = unknown>() {
   const [loading, setLoading] = useState(false);
   const [response, setResponse] = useState<RestResponse<T> | null>(null);
   const [error, setError] = useState<string | null>(null);
-
+  const supabase = createClient();
   async function sendRequest(url: string, method: string, headers: HeadersInit, body?: BodyInit) {
     setLoading(true);
     setError(null);
@@ -29,12 +30,35 @@ export function useRestClient<T = unknown>() {
       } else {
         data = await res.text();
       }
-
-      setResponse({
+      const result = {
         status: res.status,
         ok: res.ok,
         data,
-      });
+      };
+
+      setResponse(result);
+
+      const {
+        data: { user },
+        error: userError,
+      } = await supabase.auth.getUser();
+      if (userError) {
+        throw new Error(`Error: ${userError.message}`);
+      }
+      if (user === null) return;
+      const { error: insertError } = await supabase.from('rest_history').insert([
+        {
+          user_id: user.id,
+          status: result.status,
+          ok: result.ok,
+          method,
+          url,
+          data: result.data,
+        },
+      ]);
+      if (insertError) {
+        throw new Error(`Error: ${insertError.message}`);
+      }
     } catch (err: unknown) {
       setError(err instanceof Error ? err.message : String(err));
     } finally {
---
+++ src/pages/History/History.tsx
@@ -0,0 +1,91 @@
+import { getServerClient } from '@/api/server';
+import type { Route } from './+types/History';
+import { redirect, type LoaderFunctionArgs } from 'react-router';
+import { Textarea } from '@/components/ui/textarea';
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const supabase = getServerClient(request);
+  const {
+    data: { user },
+  } = await supabase.client.auth.getUser();
+
+  if (!user) {
+    return redirect('/');
+  }
+
+  const { data, error } = await supabase.client
+    .from('rest_history')
+    .select('*')
+    .eq('user_id', user.id)
+    .order('timestamp', { ascending: false });
+  if (error) {
+    throw new Response('Error with load', { status: 500 });
+  }
+
+  return { data };
+}
+
+export default function History({ loaderData }: Route.ComponentProps) {
+  return (
+    <section
+      className="flex h-1/1 w-1/1 min-w-1/1 flex-col items-start gap-6 px-8 py-6"
+      style={{
+        backgroundImage: `url('images/rest-client-mountains.jpg')`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}>
+      <h2 className="text-primary text-4xl font-semibold">History</h2>
+      <div className="w-1/1 space-y-5">
+        {loaderData.data.map(({ method, timestamp, url, data }, index) => {
+          const date = new Intl.DateTimeFormat('ru-RU', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          }).format(new Date(timestamp));
+
+          return (
+            <div
+              key={index}
+              className="flex max-h-[900px] flex-col gap-4 overflow-auto rounded-lg border border-[var(--card-foreground)] bg-gradient-to-b p-6 shadow-lg backdrop-blur-sm">
+              <div className="mb-6 last:mb-0">
+                <div>
+                  <h3 className="text-primary mb-2 text-base font-medium">Method: {method}</h3>
+                  <h4 className="text-primary/50 mb-2 text-sm">{date}</h4>
+                </div>
+                <ul>
+                  <li className="flex items-center space-x-2">
+                    <label className="font-medium" htmlFor={`endpoint-${index}`}>
+                      Endpoint URL
+                    </label>
+                    <Textarea
+                      className="border-primary h-full min-h-7 w-[350px] min-w-[350px] resize rounded border bg-white/30 pl-1 text-sm"
+                      name={`endpoint-${index}`}
+                      id={`endpoint-${index}`}
+                      readOnly
+                      value={url}
+                    />
+                  </li>
+                  <li className="mt-4 flex items-center space-x-2">
+                    <label className="font-medium" htmlFor={`response-${index}`}>
+                      Response
+                    </label>
+                    <Textarea
+                      className="border-primary h-full min-h-7 w-[350px] min-w-[350px] resize rounded border bg-white/30 pl-1 text-sm"
+                      name={`response-${index}`}
+                      id={`response-${index}`}
+                      readOnly
+                      value={JSON.stringify(data, null, 2)}
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
