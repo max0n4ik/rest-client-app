c73a5fe6e005c0d58e7a981c1c5d585d44074c22
@AsyaDanilova <asya_danilova@epam.com>
2025-09-21 00:40:35 +0300

    fix: updated route for notFound page

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { type RouteConfig, index, layout, route } from '@react-router/dev/routes
 export default [
   layout('./pages/Layout/Layout.tsx', [
     index('./pages/Home/Home.tsx'),
-    route('*', './pages/NotFound.tsx'),
+    route('*', './pages//NotFound/NotFound.tsx'),
     route('rest-client', 'pages/RestfulClient/restfulClient.tsx'),
     route('login', './pages/Login/Login.tsx'),
     route('registration', './pages/Registration/Registration.tsx'),
